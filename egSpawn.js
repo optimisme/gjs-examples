e085320b5cd014f93d0ecd8df76e305168809578
Albert <optimisme@gmail.com>
2015-02-25 00:08:12 +0100

    Fixed spawn example title

--- egSpawn.js
+++ egSpawn.js
@@ -40,7 +40,7 @@ App.prototype.buildUI = function() {
     let scroll;
 
     this.window = new Gtk.ApplicationWindow({ application: this.application,
-                                              title: "Example Icon" });
+                                              title: "Example Spawn" });
     this.window.set_default_size(200, 200);
     try {
         this.window.set_icon_from_file('./assets/app-icon.png');
