7d983e54a97c57191728c21e1f77a27bab65842a
Albert Palacios <apalacios@modpow.es>
2016-06-23 09:15:38 +0200

    Added borderless example

---
+++ egBorderless.js
@@ -0,0 +1,81 @@
+#!/usr/bin/gjs
+
+/*
+GJS example showing how to build Gtk borderless javascript 
+applications
+
+Run it with:
+    gjs egAsset.js
+*/
+
+const Gio   = imports.gi.Gio;
+const GLib  = imports.gi.GLib;
+const Gtk   = imports.gi.Gtk;
+const Lang  = imports.lang;
+
+// Get application folder and add it into the imports path
+function getAppFileInfo() {
+    let stack = (new Error()).stack,
+        stackLine = stack.split('\n')[1],
+        coincidence, path, file;
+
+    if (!stackLine) throw new Error('Could not find current file (1)');
+
+    coincidence = new RegExp('@(.+):\\d+').exec(stackLine);
+    if (!coincidence) throw new Error('Could not find current file (2)');
+
+    path = coincidence[1];
+    file = Gio.File.new_for_path(path);
+    return [file.get_path(), file.get_parent().get_path(), file.get_basename()];
+}
+const path = getAppFileInfo()[1];
+imports.searchPath.push(path);
+
+const App = function () { 
+    this.title = 'Example Asset';
+    GLib.set_prgname(this.title);
+
+    this.button;
+};
+
+App.prototype.run = function (ARGV) {
+
+    this.application = new Gtk.Application();
+    this.application.connect('activate', () => { this.onActivate(); });
+    this.application.connect('startup', () => { this.onStartup(); });
+    this.application.run([]);
+};
+
+App.prototype.onActivate = function () {
+
+    this.window.show_all();
+};
+
+App.prototype.onStartup = function() {
+
+    this.buildUI();
+};
+
+App.prototype.buildUI = function() {
+
+    this.window = new Gtk.ApplicationWindow({ application: this.application,
+                                              title: this.title,
+                                              default_height: 200,
+                                              default_width: 200,
+                                              window_position: Gtk.WindowPosition.CENTER });
+    this.window.set_decorated(false);
+
+    try {
+        this.window.set_icon_from_file(path + '/assets/appIcon.png');
+    } catch (err) {
+        this.window.set_icon_name('application-x-executable');
+    }
+
+    this.button = new Gtk.Button({ label: "Close" });
+    this.button.connect ('clicked', () => { this.window.close(); });
+    this.window.add(this.button);
+};
+
+//Run the application
+let app = new App();
+app.run(ARGV);
